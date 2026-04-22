3ed2c9bae78705c6b3b2a61cd7c2d27e5f911532
formachi-uz <timonmlbb@gmail.com>
2026-04-22 22:37:18 +0500

    Delete run_bot.sh

--- run_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Railway da ikki xil servis yaratish kerak:
-# 1) Bot servis:  python bot/main.py
-# 2) Web servis:  python run_web.py
-
-echo "🚀 Formachi Bot ishga tushirilmoqda..."
-python bot/main.py
